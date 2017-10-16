8547a0a73b6151ba32ea976233e1e9f376cbe9c3
geoffcraz <geoffcraz@gmail.com>
2017-10-16 15:50:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function nestedTarget(){
 function deepestChild(){
 return document.querySelector('#grand-node div div div div')
 }
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
